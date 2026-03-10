aac4755579b548fceb15b330a1aa91f9a811ee33
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-10 22:32:28 +0000

    monday

--- python-week1/day1.py
+++ python-week1/day1.py
@@ -1,1 +1,18 @@
-print ("hello world")
+number1  = int(input ("enter the 1st number : "))
+number2 = int(input ("enter the 2nd number : "))
+
+operator = input ("choose the operator +, * , / , -  =  ")
+
+if operator == "+" :
+    print(number1 + number2)
+
+elif operator  == "*":
+    print(number1 * number2)
+
+elif operator  == "*":
+    print(number1 * number2)
+
+else:
+    print(number1 / number2)
+
+
