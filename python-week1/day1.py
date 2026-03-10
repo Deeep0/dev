53962d538938924bb60f5835dbf0e767cf8218a0
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-10 22:43:48 +0000

    monday - a simple calculator

--- python-week1/day1.py
+++ python-week1/day1.py
@@ -2,17 +2,20 @@ number1  = int(input ("enter the 1st number : "))
 number2 = int(input ("enter the 2nd number : "))
 
 operator = input ("choose the operator +, * , / , -  =  ")
+try:
 
-if operator == "+" :
-    print(number1 + number2)
+    if operator == "+" :
+        print(number1 + number2)
 
-elif operator  == "*":
-    print(number1 * number2)
+    elif operator  == "*":
+        print(number1 * number2)
 
-elif operator  == "*":
-    print(number1 * number2)
-
-else:
-    print(number1 / number2)
+    elif operator  == "*":
+        print(number1 * number2)
 
 
+    else:
+        print(number1 / number2)
+
+except ZeroDivisionError :
+    print("can not be divided by zero")
