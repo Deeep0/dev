f04e76bb048001cd1acccefe044bc6a07daffcd1
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-17 03:51:10 +0000

    classes in oops

--- python-week1/thursday/exercise/test.py
+++ python-week1/thursday/exercise/test.py
@@ -1,15 +1,18 @@
 class Employee :
+ 
     def __init__ (self, name , age , salary):
         self.name = name
         self.age = age
         self.salary = salary
 
-    def salary_tax():
-        return (salary * 1.2)
+    def salary_tax(self):
+        return (self.salary * 1.2)
     
 
 emp1 = Employee("alex", 20 , 1000)
 
 emp2 = Employee("bat", 22 , 1200)
 print(emp1.name)
-print(emp2.pay)
+print(emp2.salary)
+
+print(emp2.salary_tax())
