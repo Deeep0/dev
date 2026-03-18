e49a77e0cfcfb055067468d31c73733886c84a04
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-18 04:21:12 +0000

    javascript

---
+++ javascript-week2/monday/script.js
@@ -0,0 +1,20 @@
+// alert("i am javascript")
+
+// alert(3+1+5)
+
+/* let message;
+message = "hello"
+
+alert(message) */
+
+
+/* let name = "john";
+let admin = name;
+
+alert(admin) */
+
+const greet = (name) => {
+    return `hello ${name}. how are you?`
+}
+
+console.log(greet("alex"))
