067bf450710db1f633361bf72e6f34306990ea92
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-21 19:02:24 +0000

    if else'

---
+++ javascript-week2/exercise5.js
@@ -0,0 +1,27 @@
+let x = 10;
+let y = 6
+
+if (x > 10) {
+    console.log("x is greater");
+}
+else if (x < 10 ){
+    console.log("x is smaller");
+}
+else {
+    console.log("x is 10");
+}
+
+
+if (x === 10 && y === 6) {
+    console.log("x is 10 and y is 6")
+}
+
+//ternary operator
+
+const z = 11;
+
+// ? is terniary
+const color = z > 10 ? 'red' : 'blue';
+
+console.log(color)
+
