36dca09d2b83b89d2b1c8ddae03a4b214bf11ab3
Deeep0 <109335211+Deeep0@users.noreply.github.com>
2026-03-21 18:46:38 +0000

    for each , maps, filter

--- javascript-week2/exercise4.js
+++ javascript-week2/exercise4.js
@@ -20,3 +20,32 @@ console.log(todos[1].text);
 
 const todoJSON = JSON.stringify(todos);
 console.log(todoJSON);
+
+for(let i = 0 ; i < todos.length; i++)
+{
+    console.log(todos[i].text)
+};
+
+for (let todo of todos){
+    console.log(todo.text)
+}
+
+//foreach
+todos.forEach(function(todo){
+    console.log(todo.text);
+})
+
+//map
+const todotext = todos.map(function(todo) {
+    return todo.text;
+})
+
+//filter
+
+const todotrue = todos.filter(function(todo){
+    return todo.iscompleted === true;
+}).map(function(todo){
+    return todo.text;
+})
+
+console.log(todotrue);
